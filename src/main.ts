52abfe91d3e8f426eefd291f8535574e7034a9d9
saiganesh.chinta <saiganesh.chinta@realvariable.com>
2024-10-22 19:08:36 +0530

    updated_CORS

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,11 @@ app.use(cors());
 
 /**
  * findComponentMaster APi call will get all the Component Master details
- */
+//  */
 app.get(
-  "/componentMaster/findAllComponentMaster",
+  "/componentMaster/findComponentMaster",
   (req: Request, res: Response) => {
+<<<<<<< Updated upstream
     res.status(200).send([
       {
         componentMasterId: "CM-001",
@@ -144,6 +145,60 @@ app.put(
     }
   }
 );
+=======
+    res.status(200).send({
+      componentMasterId: "CM-001",
+      componentMasterName: "Keyboard",
+      componentMasterDescription: "Used for Vehivles",
+      componentImage: "https://images.com/tyre.png",
+      isFinalProduct: "No",
+      category: "Rubber",
+      quantity: 100,
+      productionStatus: "Active",
+      createdBy: "miway",
+      createdOn: "2024-10-18T00:00:00.000Z",
+      updatedOn: "2024-10-18T00:00:00.000Z",
+      _id: "67122520a5768407fca14e22",
+    });
+  }
+);
+
+// POST API to create a new ComponentMaster
+app.post("/createComponentMaster", (req: Request, res: Response) => {
+  const {
+    componentMasterId,
+    componentMasterName,
+    componentMasterDescription,
+    componentImage,
+    isFinalProduct,
+    category,
+    quantity,
+    productionStatus,
+    createdBy,
+  } = req.body;
+
+  // Here you could add logic to save the component in a database
+  // For now, we are just simulating the creation and returning the same data back.
+
+  const newComponentMaster = {
+    componentMasterId: componentMasterId || "CM-002",
+    componentMasterName,
+    componentMasterDescription,
+    componentImage,
+    isFinalProduct,
+    category,
+    quantity,
+    productionStatus,
+    createdBy,
+    createdOn: new Date().toISOString(),
+    updatedOn: new Date().toISOString(),
+  };
+
+  // Responding with the created component
+  res.status(201).send(newComponentMaster);
+});
+
+>>>>>>> Stashed changes
 /**
  * findInventory APi call will get all the Inventory  details
  */
@@ -166,6 +221,7 @@ app.get("/Inventory/findInventory", (req: Request, res: Response) => {
   ]);
 });
 
+<<<<<<< Updated upstream
 /**
  *  To findInventory APi to get from component List by component name
  */
@@ -372,6 +428,8 @@ app.post("/poorder/createpoorder", (req: Request, res: Response) => {
 
 
 //////Transaction GET API
+=======
+>>>>>>> Stashed changes
 app.get("/Transaction/findTransaction", (req: Request, res: Response) => {
   res.status(200).send([
     {
