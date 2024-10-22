e2bb173fad39122f0ba35fbd2ed19cc0d58090ac
syed.saadh <syed.saadh@realvariable.com>
2024-10-22 13:10:47 +0530

    added_InventoryMasterId_API

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ app.use(express.json());
 
 /**
  * findComponentMaster APi call will get all the Component Master details
+<<<<<<< Updated upstream
  */
 app.get(
   "/componentMaster/findAllComponentMaster",
@@ -292,6 +293,120 @@ app.get("/Transaction/findTransaction", (req: Request, res: Response) => {
       feedback: "This is good product",
     },
   ]);
+>>>>>>> Stashed changes
+=======
+//  */
+app.get(
+  "/componentMaster/findComponentMaster",
+  (req: Request, res: Response) => {
+    res.status(200).send({
+      componentMasterId: "CM-001",
+      componentMasterName: "Keyboard",
+      componentMasterDescription: "Used for Vehivles",
+      componentImage: "https://images.com/tyre.png",
+      isFinalProduct: "No",
+      category: "Rubber",
+      quantity: 100,
+      productionStatus: "Active",
+      createdBy: "miway",
+      createdOn: "2024-10-18T00:00:00.000Z",
+      updatedOn: "2024-10-18T00:00:00.000Z",
+      _id: "67122520a5768407fca14e22",
+    });
+  }
+);
+
+// POST API to create a new ComponentMaster
+app.post("/createComponentMaster", (req: Request, res: Response) => {
+  const {
+    componentMasterId,
+    componentMasterName,
+    componentMasterDescription,
+    componentImage,
+    isFinalProduct,
+    category,
+    quantity,
+    productionStatus,
+    createdBy,
+  } = req.body;
+
+  // Here you could add logic to save the component in a database
+  // For now, we are just simulating the creation and returning the same data back.
+
+  const newComponentMaster = {
+    componentMasterId: componentMasterId || "CM-002",
+    componentMasterName,
+    componentMasterDescription,
+    componentImage,
+    isFinalProduct,
+    category,
+    quantity,
+    productionStatus,
+    createdBy,
+    createdOn: new Date().toISOString(),
+    updatedOn: new Date().toISOString(),
+  };
+
+  // Responding with the created component
+  res.status(201).send(newComponentMaster);
+});
+
+/**
+ * To findInventory APi call will get all the Inventory  details
+ */
+app.get("/Inventory/findInventory",(req: Request, res: Response) => {
+    res.status(200).send([{
+      componentMasterId: "CM-001",
+      componentName: "Tyre",
+      quantity: 10,
+      userId: "xxx",
+      _id: "66faeca1d9b10bced59a7585",
+    }]);
+  }
+);
+
+/**
+ *  To findInventory APi to get from component List by component name
+ */
+
+const componentList = [
+  {
+    _id: "66fb7eea86ea2d7cf5743791",
+    componentMasterId: "1",
+    qrCode: "",
+    status: "available",
+    componentId: "COM001",
+    componentName: "Engine",
+  },
+  {
+    _id: "66fb8ffb87fb2d8df5743702",
+    componentMasterId: "2",
+    qrCode: "",
+    status: "pending",
+    componentId: "COM002",
+    componentName: "Transmission",
+  },
+];
+
+/**
+ *  To findInventory APi to get from component List by component MasterId
+ */
+
+app.get("/Inventory/findComponentsByMasterIds/:componentMasterIds", (req: Request, res: Response) => {
+  const componentMasterIds = req.params.componentMasterIds.split(','); 
+  let components;
+  if(req.params.componentMasterIds == "all"){
+    components = componentList;
+  }else
+   components = componentList.filter(item => componentMasterIds.includes(item.componentMasterId));
+
+  if (components.length > 0) {
+    res.status(200).send(components); 
+  } else {
+    res.status(404).send({
+      error: "No components found for the given componentMasterIds",
+    });
+  }
 >>>>>>> Stashed changes
 });
 
