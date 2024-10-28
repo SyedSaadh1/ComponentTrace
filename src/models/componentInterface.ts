cd14b3150dfebc3e846f9959dbb9b2339abf2479
sankeerthan.agnurkar <sankeerthan.agnurkar@realvariable.com>
2024-10-28 15:02:08 +0530

    Update componentInterface.ts

--- src/models/componentInterface.ts
+++ src/models/componentInterface.ts
@@ -1,12 +1,12 @@
 
-export interface ComponentList {
-  componentMasterId: Number,
-  createdBy: string,
-  createdOn: Date,
-  currentOwner: string,
-  qrCode: string,
-  sentToDelivery: boolean,
-  componentId: string,
-  componentName: string,
-  wareHouseLocation: string
-}
+// export interface ComponentList {
+//   componentMasterId: Number,
+//   createdBy: string,
+//   createdOn: Date,
+//   currentOwner: string,
+//   qrCode: string,
+//   sentToDelivery: boolean,
+//   componentId: string,
+//   componentName: string,
+//   wareHouseLocation: string
+// }
