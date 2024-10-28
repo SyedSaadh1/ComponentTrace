a9492588344b27d2fdcdac69b91ed47a9bad028a
Vamshi <vamshi.baja@realvariable.com>
2024-10-28 15:44:09 +0530

    Update DBConnection.ts

--- src/config/DBConnection.ts
+++ src/config/DBConnection.ts
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const url = "mongodb://devuser:devuser%402024%23@20.198.90.15:27030/componentTrace_db?authSource=admin";
-class Connect {
-    async DBConnect() {
-        try {
-            await mongoose.connect(url);
-            console.log("Connected")
-        } catch (error) {
-            console.log("Error connnecting to db")
-        }
+dotenv.config();
+
+const url =
+  "mongodb://devuser:devuser%402024%23@20.198.90.15:27030/componentTrace_db?authSource=admin";
+class Connection {
+  async connect() {
+    // Disable pluralization of model names
+    mongoose.set("strictQuery", true); // Set strict query to true
+    try {
+      await mongoose.connect(url);
+      // mongoose.pluralize(null);
+      console.log("DB Connection Established Successfully");
+    } catch (error) {
+      console.error("Connecting DB Encountered an Error: " + error);
     }
+  }
 }
 
-export default new Connect();
+export default new Connection();
