e615a470351548791192cafd18bf81b7ee905a44
syed.saadh <syed.saadh@realvariable.com>
2024-11-04 13:00:35 +0530

    Updated_changes

--- src/repository/purchaseOrderRepository.ts
+++ src/repository/purchaseOrderRepository.ts
@@ -3,6 +3,9 @@ import PurchaseOrder,{ IPurchaseOrder } from "../models/purchaseOrderModel";
 
 
 class PORepository {
+  getLastInsertedId() {
+    throw new Error("Method not implemented.");
+  }
   
   createPo(order: IPurchaseOrder) {
     return PurchaseOrder.create(order);
