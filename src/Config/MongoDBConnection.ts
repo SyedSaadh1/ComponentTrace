adb176db8b14f19fe5ba0310390e10e0e5213f9c
syed.saadh <syed.saadh@realvariable.com>
2024-11-04 11:01:42 +0530

    updated_batch_API

--- src/Config/MongoDBConnection.ts
+++ src/Config/MongoDBConnection.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
- 
 // const url = "mongodb://localhost:27030/ComponentTrace_db"
 const url = "mongodb://devuser:devuser%402024%23@20.198.90.15:27030/componentTrace_db?authSource=admin";
 // mongodb://devuser:devuser%402024%23@20.198.90.15:27030/
