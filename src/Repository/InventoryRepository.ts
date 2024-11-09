da720d27560508ee1c37f14baf127987fac4b4f8
syed.saadh <syed.saadh@realvariable.com>
2024-11-09 08:33:03 +0530

    Update InventoryRepository.ts

--- src/Repository/InventoryRepository.ts
+++ src/Repository/InventoryRepository.ts
@@ -4,7 +4,14 @@ import inventoryModel, { Inventory } from "../Models/inventoryModel";
 
 class InvRepository {
   findAllInv(filter = {}) {
-    return inventoryModel.find(filter);
+    return inventoryModel.aggregate([
+      { $match: filter },  
+      {
+        $set: {
+          quantity: { $subtract: ["$quantity", "$reserved"] } // Update quantity by subtracting reserved
+        }
+      }
+    ]);
   }
   createInv(order: Inventory) {
     return inventoryModel.create(order);
