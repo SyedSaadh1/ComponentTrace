22e8bc4a59f4a9ec286129d2f5ac21ee4a61173c
Vamshi <vamshi.baja@realvariable.com>
2024-11-15 12:17:54 +0530

    Update transactionsRepository.ts

--- src/Repository/transactionsRepository.ts
+++ src/Repository/transactionsRepository.ts
@@ -3,33 +3,44 @@ import gernerateGRN from "../AutogenerateId/AutogenerateId";
 
 class TransactionRepository {
   async createTransaction(Data: ITransactions) {
-    return await transactionsModel.create(Data);
+    try {
+      return await transactionsModel.create(Data);
+    } catch (error) {
+      throw new Error("Error in creating Transaction :" + error);
+    }
   }
   async findAllTransactions(filter = {}) {
-    return await transactionsModel.find(filter);
+    try {
+      return await transactionsModel.find(filter);
+    } catch (error) {
+      throw new Error("Error while fetching Transactions : " + error);
+    }
   }
   async updateGRNNumber(
     transactionId: string,
     grnNumber: string,
     grnData: any
   ) {
-    return await transactionsModel.updateOne(
-      { transactionId },
-      {
-        $set: {
-          grnNumber: grnNumber,
-          receivedDate: grnData.receivedDate,
-          receievedByCustomer: true,
-          transactionStatus: "Completed",
-        },
-      }
-    );
+    try {
+      return await transactionsModel.updateOne(
+        { transactionId },
+        {
+          $set: {
+            grnNumber: grnNumber,
+            receivedDate: grnData.receivedDate,
+            receievedByCustomer: true,
+            transactionStatus: "Completed",
+          },
+        }
+      );
+    } catch (error) {
+      throw new Error("Error in Creating GRN Number");
+    }
   }
   async findLatestGRN() {
     return await transactionsModel
-      .findOne({ grnNumber: { $exists: true, $ne: "" } }) // Filters for documents where grnNumber exists and is not an empty string
-      .sort({ updatedAt: -1 }) // Sorts in descending order by updatedAt (latest first)
-      .exec(); // Executes the query and returns a promise
+      .findOne({ grnNumber: { $exists: true, $ne: "" } })
+      .sort({ updatedAt: -1 });
   }
 
   async findLatestTransaction() {
